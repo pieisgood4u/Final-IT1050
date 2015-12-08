2ba3fcc553f747f2d49e36fc08d8291ed1f0e367
pieisgood4u <jacob_douglass@att.net>
2015-12-07 22:07:53 -0500

    Final

---
+++ Final/Final Project/Elevator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int MaxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[MaxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+
+            this.Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+            foreach (Passenger item in Occupants)
+            {
+                totalWeight += item.GetWeight();
+            }
+            return totalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return this.GetCurrentWeight() > this.MaxWeight;
+        }
+
+    }
+}
---
+++ Final/Final Project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator1 = new Elevator(2, 400);
+            Passenger p1 = new Passenger("A1", 180);
+            Passenger p2 = new Passenger("A2", 220);
+            elevator1.AddOccupant(p1, 0);
+            elevator1.AddOccupant(p2, 1);
+
+            bool elevator1IsOverMaxCapacity = elevator1.IsOverMaxCapacity();
+
+            Elevator elevator2 = new Elevator(3, 600);
+            Passenger p3 = new Passenger("A1", 200);
+            Passenger p4 = new Passenger("A2", 200);
+            Passenger p5 = new Passenger("A3", 201);
+            elevator2.AddOccupant(p3, 0);
+            elevator2.AddOccupant(p4, 1);
+            elevator2.AddOccupant(p5, 2);
+
+            bool elevator2IsOverMaxCapacity = elevator2.IsOverMaxCapacity();
+        }
+    }
+}
